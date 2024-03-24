9256fb30f0667840c0f166f7d5a7801311f67067
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-03-24 15:46:54 -0500

    Test file

---
+++ SOMPZ/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name Wz_test
+#SBATCH --partition=chihway
+#SBATCH --account=pi-chihway
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=30:00:00
+#SBATCH --output=/home/dhayaa/Desktop/DECADE/CosmicShearPhotoZ/SOMPZ/test.log
+#SBATCH --mail-user=dhayaa@uchicago.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --exclude=midway2-0690,midway2-0694
+
+
+RUNNER=/home/dhayaa/Desktop/DECADE/CosmicShearPhotoZ/SOMPZ/DELVERunner.py
+
+#python -u $RUNNER --TrainRunner
+#python -u $RUNNER --ClassifyRunner
+python -u $RUNNER --BinRunner
