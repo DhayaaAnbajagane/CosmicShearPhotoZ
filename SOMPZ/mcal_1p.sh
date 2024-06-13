0be74a5326bc9eba5da5fa2e0a483647c0131acb
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-06-13 14:36:40 -0500

    More jobs

---
+++ SOMPZ/mcal_1p.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name mcal_1p_SOM
+#SBATCH --partition=chihway
+#SBATCH --account=pi-chihway
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --time=60:00:00
+#SBATCH --output=/home/dhayaa/Desktop/DECADE/CosmicShearPhotoZ/SOMPZ/log_%x
+#SBATCH --mail-user=dhayaa@uchicago.edu
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --exclude=midway2-0694
+
+
+RUNNER=/home/dhayaa/Desktop/DECADE/CosmicShearPhotoZ/SOMPZ/DELVERunner.py
+RUNNER2=/home/dhayaa/Desktop/DECADE/CosmicShearPhotoZ/SOMPZ/ThreeSDirRunner.py
+
+python -u $RUNNER --AllMcalRunner --MCAL_TYPE 1p --njobs 15
