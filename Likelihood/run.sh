62c1109025a99db5e0bb575d3e0ebad05bf1ec01
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-09-01 16:50:08 -0500

    New likelihood eval run

--- Likelihood/run.sh
+++ Likelihood/run.sh
@@ -13,34 +13,34 @@
 RUNNER=/home/dhayaa/Desktop/DECADE/CosmicShearPhotoZ/Likelihood/LikelihoodRunner.py
 
 
-python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240408/Final/ \
-                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240726_fiducial_Wz_600patch_urmask_CovMat.npy\
+python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240831/Final/ \
+                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240831_fiducial_Wz_600patch_urmask_CovMat.npy\
                   --njobs 16 \
                   --b_u 1 --db_u 1.5 --alpha_u 1.9 --dalpha_u 1 \
                   --M 5 --rms 0.15 --tol 0.05 --Niter 30 \
-                  --Name Fiducial
+                  --Name Fiducial_all
          
 
-python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240408/Final/ \
-                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240726_fiducial_Wz_600patch_urmask_CovMat.npy\
+python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240831/Final/ \
+                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240831_fiducial_Wz_600patch_urmask_CovMat.npy\
                   --njobs 16 \
                   --b_u 1 --db_u 6.0 --alpha_u 1.9 --dalpha_u 4 \
                   --M 5 --rms 0.15 --tol 0.05 --Niter 30 \
-                  --Name Fiducial_HighUncert_balpha_u
+                  --Name Fiducial_HighUncert_balpha_u_all
                   
                   
-python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240408/Final/ \
-                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240726_fiducial_Wz_600patch_urmask_CovMat.npy\
+python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240831/Final/ \
+                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240831_fiducial_Wz_600patch_urmask_CovMat.npy\
                   --njobs 16 \
                   --b_u 1 --db_u 1.5 --alpha_u 1.9 --dalpha_u 1 \
                   --M 5 --rms 0.6 --tol 0.05 --Niter 30 \
-                  --Name Fiducial_HighUncert_Sys
+                  --Name Fiducial_HighUncert_Sys_all
                   
                   
-python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240408/Final/ \
-                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240726_fiducial_Wz_600patch_urmask_CovMat.npy\
+python -u $RUNNER --NzDir /project/chihway/dhayaa/DECADE/SOMPZ/Runs/20240831/Final/ \
+                  --WzPath /project/chihway/dhayaa/DECADE/Wz/20240831_fiducial_Wz_600patch_urmask_CovMat.npy\
                   --njobs 16 \
                   --b_u 1 --db_u 1.5 --alpha_u 1.9 --dalpha_u 1 \
                   --M 10 --rms 0.15 --tol 0.05 --Niter 30 \
-                  --Name Fiducial_HighOrder_Sys
+                  --Name Fiducial_HighOrder_Sys_all
                   
