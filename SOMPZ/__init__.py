75f3c6c4599f57e93ac49fc2fd54aef086a392b6
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2024-10-26 13:42:08 -0500

    Add init

--- SOMPZ/__init__.py
+++ SOMPZ/__init__.py
@@ -1,5 +1,5 @@
 from .NoiseSOM import NoiseSOM
 from .DELVERunner import TrainRunner, ClassifyRunner, BinRunner
-from .ThreeSDirRunner import ThreeSDirRunner
+from .ThreeSDirRunner import ThreeSDirRunner, ThreeSDirRedbiasRunner
 from .SOM import ClassifySOM, TrainSOM
 from .Files import my_files
