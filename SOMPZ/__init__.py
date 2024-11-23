31b32a38ba270a6fd11c881667a4d90fabac067b
DhayaaAnbajagane <dhayaaanbajagane@gmail.com>
2024-11-22 21:45:08 -0600

    Access to catalog runner

--- SOMPZ/__init__.py
+++ SOMPZ/__init__.py
@@ -3,3 +3,4 @@ from .DELVERunner import TrainRunner, ClassifyRunner, BinRunner
 from .ThreeSDirRunner import ThreeSDirRunner, ThreeSDirRedbiasRunner
 from .SOM import ClassifySOM, TrainSOM
 from .Files import my_files
+from .RedshiftSampleRunner import generate_catalog_and_bias
